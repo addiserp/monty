9074a15530f7ea1bc670562dd52e6fc9b601df1b
mikias gedlu <addiserp@gmail.com>
2022-12-15 17:16:24 +0300

    0x19. C - Stacks, Queues - LIFO, FIFO 0. push, pall 2. pop 3. swap

--- opcodes_2.c
+++ opcodes_2.c
@@ -89,3 +89,29 @@ void pstr(stack_t **stack, unsigned int nline)
 	}
 	printf("\n");
 }
+/**
+ * pop - delets the top element
+ * @stack: pointer to the head
+ * @nline: the line number
+ * Return: void.
+ */
+void pop(stack_t **stack, unsigned int nline)
+{
+	if (stack == NULL || *stack == NULL)
+	{
+		fprintf(stderr, "L%d: can't pop an empty stack\n", nline);
+		exit(EXIT_FAILURE);
+	}
+	/* if stack is more than 1 node, else free entire thing */
+	if ((*stack)->next != NULL)
+	{
+		*stack = (*stack)->next;
+		free((*stack)->prev);
+		(*stack)->prev = NULL;
+	}
+	else
+	{
+		free(*stack);
+		*stack = NULL;
+	}
+}
