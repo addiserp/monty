693637f9bf2db5e28bfa8bfcfadb913f7e38449c
Mikias Gedlu <addiserp@gmail.com>
2023-08-23 20:02:25 +0300

    monty all

--- monty.h
+++ monty.h
@@ -1,6 +1,6 @@
 #ifndef MONTY_H
 #define MONTY_H
-
+#define  _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -13,6 +13,7 @@
  
 
 extern FILE *file;
+FILE *file;
 
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
